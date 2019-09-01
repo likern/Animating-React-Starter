25100654bdc10fa538932da5bf84fd19d4094352
Malov Victor <malvikus@gmail.com>
2019-09-01 18:42:44 +0300

    useTransition hook with list

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,23 +1,53 @@
 import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
-    from: { opacity: 0, position: "absolute" },
+  const [items, setItems] = useState([
+    {
+      letter: "S",
+      key: 1
+    },
+    {
+      letter: "c",
+      key: 2
+    },
+    {
+      letter: "o",
+      key: 3
+    },
+    {
+      letter: "t",
+      key: 4
+    },
+    {
+      letter: "t",
+      key: 5
+    }
+  ]);
+  const transition = useTransition(items, item => item.key, {
+    from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
   });
   return (
     <div>
-      {transition.map(({ item, key, props }) =>
-        item ? (
-          <animated.h1 style={props}>Hello</animated.h1>
-        ) : (
-          <animated.h1 style={props}>World</animated.h1>
-        )
-      )}
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
 
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: "S",
+              key: 1
+            }
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
